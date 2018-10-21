7d91358eebbe1518d136d862a5875d48a19014fb
Darren <darrenma1986@gmail.com>
2018-10-21 17:07:19 +0200

    2. List Candidates finished

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,27 @@
 pragma solidity ^0.4.24;
 
 contract Election {
-    // Store candidate
-    // Read canditdate
-    string public candidate;
-    // Constructor
+    // Model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Store Candidates
+
+    // Fetch Candidate
+    mapping (uint => Candidate) public candidates;
+    // Store Candidates count
+    uint public candidatesCount;
+
     constructor () public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
